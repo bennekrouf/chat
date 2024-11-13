84456c0b51237f9dcbe7dc51ff5e27eaef16a947
Bennekrouf <mohamed.bennekrouf@gmail.com>
2024-11-13 19:49:03 +0100

    Change api call

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -30,7 +30,7 @@ export async function POST(request: Request): Promise<Response> {
 
     // Create client
     const client = new proto.matcher.Matcher(
-      'chat.mayorana.ch:50030',
+      'matcher.mayorana.ch:8081',
       grpc.credentials.createInsecure()
     );
 
